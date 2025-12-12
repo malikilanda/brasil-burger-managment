ef8d0e8f9567015bdd133ad2f778d2100ad4551d
malikilanda <ilandaafo.malik@yahoo.com>
2025-12-12 17:20:04 +0000

    add database connecion logic

--- java/src/main/java/com/brasilburger/Main.java
+++ java/src/main/java/com/brasilburger/Main.java
@@ -1,7 +1,17 @@
 package com.brasilburger;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from Main!");
+        try (Connection connection = DatabaseConnection.getConnection()) {
+            if (connection != null) {
+                System.out.println("Connexion à la base de données réussie !");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur de connexion à la base de données :");
+            e.printStackTrace();
+        }
     }
 }
