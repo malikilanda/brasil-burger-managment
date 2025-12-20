19d48e776a303cc94e0ea175ce300c341ae9911e
malikilanda <ilandaaffo.malik@yahoo.com>
2025-12-20 19:05:01 +0000

    reglages

--- csharp/BrasilBurger/Program.cs
+++ csharp/BrasilBurger/Program.cs
@@ -1,60 +1,26 @@
 using BrasilBurger.Data;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
-using NpgsqlTypes;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 
-// ✅ Connection string (Render-friendly)
-string? cs = builder.Configuration.GetConnectionString("DefaultConnection");
-
-if (string.IsNullOrWhiteSpace(cs))
-{
-    var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-    if (!string.IsNullOrWhiteSpace(databaseUrl))
-    {
-        var uri = new Uri(databaseUrl);
-        var userInfo = uri.UserInfo.Split(':', 2);
-
-        var npg = new NpgsqlConnectionStringBuilder
-        {
-            Host = uri.Host,
-            Port = uri.Port,
-            Username = userInfo[0],
-            Password = userInfo.Length > 1 ? userInfo[1] : "",
-            Database = uri.AbsolutePath.TrimStart('/'),
-            SslMode = SslMode.Require,
-            TrustServerCertificate = true
-        };
-
-        cs = npg.ConnectionString;
-    }
-}
-
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(cs)
-);
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Auth cookie
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+builder.Services.AddAuthentication("Cookies")
+    .AddCookie("Cookies", opt =>
     {
-        options.LoginPath = "/";
-        options.AccessDeniedPath = "/";
+        opt.LoginPath = "/Index";
+        opt.LogoutPath = "/Logout";
     });
 
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
+app.UseExceptionHandler("/Error");
+app.UseHsts();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -65,4 +31,5 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
 app.Run();
