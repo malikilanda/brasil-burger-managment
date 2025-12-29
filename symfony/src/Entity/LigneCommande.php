b6aacb277bad457e71595064414b5b2bc1af7573
malikilanda <ilandaaffo.malik@yahoo.com>
2025-12-29 18:54:11 +0000

    Création entité Complement

---
+++ symfony/src/Entity/LigneCommande.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LigneCommandeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LigneCommandeRepository::class)]
+class LigneCommande
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ symfony/src/Entity/Zone.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ZoneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ZoneRepository::class)]
+class Zone
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
